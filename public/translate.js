0254f73fa12f8d73a843f4f965ddae66a4bb68bc
Luca-Liseros-Ferrari <90519432+Luca-Liseros-Ferrari@users.noreply.github.com>
2024-04-21 23:03:44 +0200

    Primo commit

---
+++ public/translate.js
@@ -0,0 +1,84 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  // select
+  let selectLang = document.getElementById("selectLang");
+
+  // options
+  let languages = document.querySelectorAll(".languages");
+
+  // handler
+  selectLang.addEventListener("change", changeHandler);
+
+  let allElements = document.querySelectorAll("*:not(:empty)");
+  console.log(allElements);
+
+  // Oggetto per memorizzare le traduzioni
+  let translations = {};
+
+  async function changeHandler(e) {
+    let targetLanguage;
+    languages.forEach(function (item) {
+      let selectedValue = e.target.value;
+      if (selectedValue === "italiano") {
+        targetLanguage = "it";
+        return targetLanguage;
+      }
+      if (selectedValue === "english") {
+        targetLanguage = "en";
+        return targetLanguage;
+      }
+      if (selectedValue === "fr") {
+        targetLanguage = "fr";
+        return targetLanguage;
+      }
+      console.log(item);
+    });
+
+    async function translateText(textNode) {
+      let text = textNode.nodeValue;
+      let translationKey = `${text}_${targetLanguage}`;
+      // Controlla se la traduzione è già memorizzata
+      if (translations.hasOwnProperty(translationKey)) {
+        textNode.nodeValue = translations[translationKey];
+        return;
+      }
+      fetch(
+        `http://localhost:3000/translate?text=${encodeURIComponent(
+          textNode.nodeValue
+        )}&target=${encodeURIComponent(targetLanguage)}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          let translatedText = data.translation;
+          textNode.nodeValue = translatedText;
+          console.log(translatedText);
+          // Memorizza la traduzione
+          translations[translationKey] = translatedText;
+        })
+        .catch((error) => {
+          console.error("Translation error:", error);
+        });
+    }
+
+    async function traverseNodes(node) {
+      node.childNodes.forEach(function (item) {
+        if (
+          item.nodeType === Node.TEXT_NODE &&
+          item.textContent.trim() !== ""
+        ) {
+          translateText(item);
+        }
+      });
+    }
+    for (let i = 0; i < allElements.length; i++) {
+      allElements[i].classList.add("translateClass");
+      traverseNodes(allElements[i]);
+    }
+
+    /*
+    const elementsWithText = document.querySelectorAll(".translateClass");
+    elementsWithText.forEach((element) => {
+      traverseNodes(element);
+    });
+    */
+  }
+});
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const translate = require('translate-google');
+const path = require("path");
+
+let app = express();
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname,'public')))
+app.get('/',(req, res)=>{
+  res.sendFile('index.html')
+})
+
+app.get('/translate', async (req, res) => {
+  const text = req.query.text;
+  const target = req.query.target;
+
+  try {
+    const translation = await translate(text, { to: target });
+    res.json({ translation });
+  } catch (error) {
+    res.status(500).json({ error: 'Translation error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Express is running on port ${port}`);
+});
